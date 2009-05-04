ea03f2a75f4d12d8b953a603461d0dff0f1f80aa
spamsch <unknown>
2009-05-04 17:19:36 +0000

    Released 0.9.1 before applying patches

--- trunk/setup.py
+++ trunk/setup.py
@@ -11,40 +11,40 @@ from setuptools import setup
 VERSION = open('VERSION', 'r').read()
 VERSION = VERSION.replace('\n', '')
 
+DOC = """
+WebDAV library for python. Consists of a server and the DAV package that provides WebDAV functionality.
+After installation of this package you will have a new script in you $PYTHON/bin directory called
+*davserver*. This serves as the main entry point to the server.
+
+Example ::
+    davserver -D /home/files -n
+
+For more information go to http://code.google.com/p/pywebdav/
+"""
+
 from distutils.core import setup
 setup(name='PyWebDAV',
-      description='WebDAV library and server for python',
+      description='WebDAV library including a standalone server for python',
       author='Simon Pamies',
       author_email='spamsch@gmail.com',
       maintainer='Simon Pamies',
       maintainer_email='spamsch@gmail.com',
       url='http://code.google.com/p/pywebdav/',
       platforms=['Unix', 'Windows'],
-      license='GPL',
+      license='GPL v2',
       version=VERSION,
-      long_description="""
-      WebDAV library for python. Consists of a server and the DAV package that provides WebDAV functionality.
-      After installation of this package you will have a new script in you $PYTHON/bin directory called
-      *davserver*. This serves as the main entry point to the server.
-
-      Example
-      ::
-        davserver -D /home/files -n
-
-      For more information go to http://code.google.com/p/pywebdav/
-      """,
+      long_description=DOC,
       classifiers = [
           'Development Status :: 5 - Production/Stable',
           'Environment :: Console',
           'Environment :: Web Environment',
-          'Intended Audience :: End Users',
           'Intended Audience :: Developers',
           'Intended Audience :: System Administrators',
           'License :: OSI Approved :: GNU General Public License (GPL)',
           'Operating System :: MacOS :: MacOS X',
           'Operating System :: POSIX',
           'Programming Language :: Python',
-          'Classifier: Topic :: Software Development :: Libraries'
+          'Topic :: Software Development :: Libraries'
           ],
       keywords = ['webdav',
                   'server',

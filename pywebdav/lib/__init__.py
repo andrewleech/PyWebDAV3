90b5d612224f5d7b70c430057ea9d769d6f4cf68
Andrew Leech <andrew@alelec.net>
2016-07-21 07:51:46 +1000

    Minor fix for change in package name.

--- pywebdav/lib/__init__.py
+++ pywebdav/lib/__init__.py
@@ -3,8 +3,8 @@ from __future__ import absolute_import
 import pkg_resources
 
 # get version from package
-package = pkg_resources.require('PyWebDAV')[0]
+package = pkg_resources.require('PyWebDAV3')[0]
 VERSION = package.version
 
 # author hardcoded here
-AUTHOR = 'Simon Pamies (spamsch@gmail.com)'
+AUTHOR = 'Andrew Leech <andrew@alelec.net>, Simon Pamies <spamsch@gmail.com>'

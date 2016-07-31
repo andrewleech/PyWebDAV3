a88fceb3c093543b84c372f027c96f3ed6332a3b
Andrew Leech <andrew@alelec.net>
2016-08-01 08:08:03 +1000

    Remove dependency on PyWebDAV3 package being install to run

--- pywebdav/lib/__init__.py
+++ pywebdav/lib/__init__.py
@@ -2,9 +2,13 @@
 from __future__ import absolute_import
 import pkg_resources
 
-# get version from package
-package = pkg_resources.require('PyWebDAV3')[0]
-VERSION = package.version
+# try to get version from package (if installed)
+try:
+    package = pkg_resources.require('PyWebDAV3')[0]
+    VERSION = package.version
+except pkg_resources.DistributionNotFound:
+    # Not running from installed version
+    VERSION = "DEVELOPMENT"
 
 # author hardcoded here
 AUTHOR = 'Andrew Leech <andrew@alelec.net>, Simon Pamies <spamsch@gmail.com>'

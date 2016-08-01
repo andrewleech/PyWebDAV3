65c6d65b0eab2809f8c1c7df184bf726231b85bf
Andrew Leech <andrew@alelec.net>
2016-08-01 22:51:56 +1000

    restructure test

--- test/test_litmus.py
+++ test/test_litmus.py
@@ -3,9 +3,9 @@ import os
 import sys
 import time
 import shutil
-import signal
 import tarfile
 import tempfile
+import unittest
 import subprocess
 
 testdir = os.path.abspath(os.path.dirname(__file__))
@@ -13,29 +13,29 @@ sys.path.insert(0, os.path.join(testdir, '..'))
 
 import pywebdav.server.server
 
-def test_run_litmus():
+# Run davserver
+user = 'test'
+password = 'pass'
+port = 38028
 
-    # Run davserver
-    user = 'test'
-    password = 'pass'
-    port = 38028
-    rundir = tempfile.mkdtemp()
-    result = []
-    proc = None
-    try:
-        print('Starting davserver')
-        davserver_cmd = [sys.executable, os.path.join(testdir,'..','pywebdav','server','server.py'),'-D', rundir, '-u', user, '-p', password, '-H', 'localhost', '--port', str(port)]
-        proc = subprocess.Popen(davserver_cmd)
 
-        #  Compile litmus
+class Test(unittest.TestCase):
+    def setUp(self):
+
+        self.rundir = tempfile.mkdtemp()
+        self._start_davserver()
+        self._ensure_litmus()
+
+    def _ensure_litmus(self):
+
         litmus_dist = os.path.join(testdir, 'litmus-0.13')
-        litmus = os.path.join(litmus_dist, 'litmus')
-        if not os.path.exists(litmus):
+        self.litmus = os.path.join(litmus_dist, 'litmus')
+        if not os.path.exists(self.litmus):
             print('Compiling litmus test suite')
 
             if os.path.exists(litmus_dist):
                 shutil.rmtree(litmus_dist)
-            with tarfile.open(litmus_dist+'.tar.gz') as tf:
+            with tarfile.open(litmus_dist + '.tar.gz') as tf:
                 tf.extractall(path=testdir)
             ret = subprocess.call(['sh', './configure'], cwd=litmus_dist)
             assert ret == 0
@@ -43,26 +43,40 @@ def test_run_litmus():
             assert ret == 0
             litmus = os.path.join(litmus_dist, 'litmus')
             assert os.path.exists(litmus)
-        else:
-            # Ensure davserver has time to start
-            time.sleep(1)
 
-        # Run Litmus
-        print('Running litmus')
+    def _start_davserver(self):
+        print('Starting davserver')
+        davserver_cmd = [sys.executable, os.path.join(testdir, '..', 'pywebdav', 'server', 'server.py'), '-D',
+                         self.rundir, '-u', user, '-p', password, '-H', 'localhost', '--port', str(port)]
+        self.davserver_proc = subprocess.Popen(davserver_cmd)
+        # Ensure davserver has time to startup
+        time.sleep(1)
+
+    def tearDown(self):
+        print('Stopping davserver')
+        self.davserver_proc.kill()
+
+        print("Cleaning up tempdir")
+        shutil.rmtree(self.rundir)
+
+    def test_run_litmus(self):
+
+        result = []
+        proc = None
         try:
-            results = subprocess.check_output([litmus, 'http://localhost:%d' % port, user, password])
-        except subprocess.CalledProcessError as ex:
-            results = ex.output
-        lines = results.decode().split('\n')
-        for line in lines:
-            line = line.split('\r')[-1]
-            result.append(line)
-            if len(re.findall('^ *\d+\.', line)):
-                assert line.endswith('pass'), line
+            # Run Litmus
+            print('Running litmus')
+            try:
+                results = subprocess.check_output([self.litmus, 'http://localhost:%d' % port, user, password])
+            except subprocess.CalledProcessError as ex:
+                results = ex.output
+            lines = results.decode().split('\n')
+            assert len(lines), "No litmus output"
+            for line in lines:
+                line = line.split('\r')[-1]
+                result.append(line)
+                if len(re.findall('^ *\d+\.', line)):
+                    assert line.endswith('pass'), line
 
-    finally:
-        print('\n'.join(result))
-        if proc:
-            proc.kill()
-        # os.killpg(0, signal.SIGKILL)  # kill all processes in my group
-        shutil.rmtree(rundir)
+        finally:
+            print('\n'.join(result))

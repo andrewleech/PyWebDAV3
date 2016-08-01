3c9e5fa987cba0941c182c8aba90cf4113fe1711
Andrew Leech <andrew@alelec.net>
2016-08-01 22:01:04 +1000

    Re-order test and add debugging output. Run davserver as subprocess rather than thread.

--- test/test_litmus.py
+++ test/test_litmus.py
@@ -3,9 +3,9 @@ import os
 import sys
 import time
 import shutil
+import signal
 import tarfile
 import tempfile
-import threading
 import subprocess
 
 testdir = os.path.abspath(os.path.dirname(__file__))
@@ -15,37 +15,42 @@ import pywebdav.server.server
 
 def test_run_litmus():
 
-    #  Compile litmus
-    litmus_dist = os.path.join(testdir, 'litmus-0.13')
-    litmus = os.path.join(litmus_dist, 'litmus')
-    if not os.path.exists(litmus):
-        if os.path.exists(litmus_dist):
-            shutil.rmtree(litmus_dist)
-        with tarfile.open(litmus_dist+'.tar.gz') as tf:
-            tf.extractall(path=testdir)
-        ret = subprocess.call('./configure', shell=True, cwd=litmus_dist)
-        assert ret == 0
-        ret = subprocess.call('make', shell=True, cwd=litmus_dist)
-        assert ret == 0
-        litmus = os.path.join(litmus_dist, 'litmus')
-        assert os.path.exists(litmus)
-
     # Run davserver
     user = 'test'
     password = 'pass'
+    port = 38028
     rundir = tempfile.mkdtemp()
     result = []
+    proc = None
     try:
+        print('Starting davserver')
+        davserver_cmd = [sys.executable, os.path.join(testdir,'..','pywebdav','server','server.py'),'-D', rundir, '-u', user, '-p', password, '-H', 'localhost', '--port', str(port)]
+        proc = subprocess.Popen(davserver_cmd)
 
-        sys.argv = sys.argv[0:1] + ['-D', rundir, '-u', user, '-p', password, '-H', 'localhost', '--port', '8078']
-        davthread = threading.Thread(target=pywebdav.server.server.run)
-        davthread.daemon = True
-        davthread.start()
-        time.sleep(2)
+        #  Compile litmus
+        litmus_dist = os.path.join(testdir, 'litmus-0.13')
+        litmus = os.path.join(litmus_dist, 'litmus')
+        if not os.path.exists(litmus):
+            print('Compiling litmus test suite')
+
+            if os.path.exists(litmus_dist):
+                shutil.rmtree(litmus_dist)
+            with tarfile.open(litmus_dist+'.tar.gz') as tf:
+                tf.extractall(path=testdir)
+            ret = subprocess.call('./configure', shell=True, cwd=litmus_dist)
+            assert ret == 0
+            ret = subprocess.call('make', shell=True, cwd=litmus_dist)
+            assert ret == 0
+            litmus = os.path.join(litmus_dist, 'litmus')
+            assert os.path.exists(litmus)
+        else:
+            # Ensure davserver has time to start
+            time.sleep(1)
 
         # Run Litmus
+        print('Running litmus')
         try:
-            results = subprocess.check_output([litmus, 'http://localhost:8078', user, password])
+            results = subprocess.check_output([litmus, 'http://localhost:%d' % port, user, password])
         except subprocess.CalledProcessError as ex:
             results = ex.output
         lines = results.decode().split('\n')
@@ -57,4 +62,7 @@ def test_run_litmus():
 
     finally:
         print('\n'.join(result))
+        if proc:
+            proc.kill()
+        # os.killpg(0, signal.SIGKILL)  # kill all processes in my group
         shutil.rmtree(rundir)

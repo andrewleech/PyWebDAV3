fa391d4cb87b9ee0e8a1d2e4f60d5ea95734024e
Andrew Leech <andrew.leech@planetinnovation.com.au>
2019-09-03 10:40:47 +1000

    Update to version 0.9.14

--- pywebdav/__init__.py
+++ pywebdav/__init__.py
@@ -1,5 +1,5 @@
 """WebDAV library including a standalone server for python 3"""
-__version__ = '0.9.13'
+__version__ = '0.9.14'
 __author__ = 'Andrew Leech (previously Simon Pamies)'
 __email__ = 'andrew@alelec.net'
 __license__ = 'LGPL v2'
